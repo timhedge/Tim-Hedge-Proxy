e3ed7b1e6446da0c276407d7813b09691b4a20b2
Tim Hedge <timwhedge@gmail.com>
2020-03-30 17:11:17 -0500

    adds item info module

--- proxy/server/proxy.js
+++ proxy/server/proxy.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const port = 3001;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors());
 
 app.listen(port, () => console.log(`Proxy app listening on port ${port}!`));
